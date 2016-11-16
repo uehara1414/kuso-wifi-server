92371a4a4317e25061125f67e9f94e9dfcf3b1d3
uehara1414 <b1015062@fun.ac.jp>
2016-11-16 15:20:07 +0900

    Remove filter setting nav

--- kuso_wifi_server/urls.py
+++ kuso_wifi_server/urls.py
@@ -6,6 +6,6 @@ urlpatterns = [
     url(r'^$', views.index, name='index'),
     url(r'^honto-kuso/$', views.ajax_post),
     url(r'^(?P<year>[0-9]+)年(?P<month>[0-9]+)月(?P<day>[0-9]+)日にどれだけクソだったか$', views.one_day_report, name="one_day_kuso"),
-    url(r'^ajax-filter$', views.post_filter_settings, name='ajax-filter'),
+    url(r'^filter-ajax$', views.post_filter_settings, name='filter-ajax'),
     url(r'^ssid-ajax$', views.ssid_ajax, name='ssid-ajax'),
 ]
