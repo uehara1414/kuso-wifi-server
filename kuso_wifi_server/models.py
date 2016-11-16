1f898c4df097b75e4d0e646c2ad93b5571a95b84
uehara1414 <b1015062@fun.ac.jp>
2016-11-16 14:15:44 +0900

    Change models

--- kuso_wifi_server/models.py
+++ kuso_wifi_server/models.py
@@ -4,50 +4,60 @@ from django.db import models
 class Client(models.Model):
     uid = models.CharField(max_length=200)
 
+    @staticmethod
+    def get_or_create_new(uid):
+        try:
+            client = Client.objects.get(uid=uid)
+        except Client.DoesNotExist:
+            client = Client(uid=uid)
+            client.save()
+        return client
+
     def __str__(self):
         return self.uid
 
 
-class KusoWifi(models.Model):
-    client = models.ForeignKey(Client, on_delete=models.CASCADE)
+class Wifi(models.Model):
     ssid = models.CharField(max_length=64, default="unknown")
+
+    @staticmethod
+    def get_or_create_new(ssid):
+        try:
+            wifi = Client.objects.get(ssid=ssid)
+        except Wifi.DoesNotExist:
+            wifi = Client(ssid=ssid)
+            wifi.save()
+        return wifi
+
+    def __str__(self):
+        return self.ssid
+
+
+class WifiReport(models.Model):
+    client = models.ForeignKey(Client, on_delete=models.CASCADE)
+    wifi = models.ForeignKey(Wifi, on_delete=models.CASCADE)
     date = models.DateTimeField()
     ping_ms = models.IntegerField(default=0)
     comment = models.TextField(max_length=256)
 
     @staticmethod
     def create_new(uid, ssid, date, ping_ms, comment):
-        try:
-            client = Client.objects.get(uid=uid)
-        except Client.DoesNotExist:
-            client = Client(uid=uid)
-            client.save()
-
-        wifi = KusoWifi(client=client, ssid=ssid, date=date, ping_ms=ping_ms, comment=comment)
+        client = Client.get_or_create_new(uid)
+        wifi = Wifi.get_or_create_new(ssid=ssid)
+        wifi = WifiReport(client=client, wifi=wifi, date=date, ping_ms=ping_ms, comment=comment)
         wifi.save()
 
     def __str__(self):
-        return self.comment
+        return str(self.date) + str(self.wifi) + self.comment
 
     @staticmethod
-    def get_one_day(year, month, day):
-        return KusoWifi.objects.filter(date__year=year, date__month=month, date__day=day)
-
-
-    @staticmethod
-    def get_ssid_set():
-        ret = set()
-        for x in KusoWifi.objects.values_list("ssid"):
-            ret.add(x[0])
-        return ret
-
-
-    @staticmethod
-    def count_kuso_wifi():
+    def count_daily_wifi_report(filter_wifi=None):
+        if filter_wifi is None:
+            wifi = Wifi.objects.all()
         ret = []
-        date_set = KusoWifi.objects.dates('date', 'day')
+        date_set = WifiReport.objects.dates('date', 'day')
         for date in date_set:
-            count = len(KusoWifi.objects.filter(date__year=date.year, date__month=date.month, date__day=date.day))
+            count = len(WifiReport.objects.filter(date__year=date.year, date__month=date.month, date__day=date.day, wifi__in=filter_wifi))
             dct = {'date': date, 'count': count}
             ret.append(dct)
         return ret
