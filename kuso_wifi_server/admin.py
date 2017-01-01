9836a2a43cd48097b2b653e01c590ad06c2bab63
uehara1414 <b1015062@fun.ac.jp>
2017-01-01 22:01:28 +0900

    Delete previous structured files

--- kuso_wifi_server/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from .models import Client, Wifi, WifiReport
-# Register your models here.
-
-admin.site.register(Client)
-admin.site.register(Wifi)
-admin.site.register(WifiReport)
--- kuso_wifi_server/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class KusoWifiServerConfig(AppConfig):
-    name = 'kuso_wifi_server'
