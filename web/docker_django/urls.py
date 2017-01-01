b475cfd53ef44de9827dc063fbb8461844c28182
uehara1414 <b1015062@fun.ac.jp>
2017-01-01 22:57:06 +0900

    Enable to run

--- web/docker_django/urls.py
+++ web/docker_django/urls.py
@@ -3,6 +3,6 @@ from django.contrib import admin
 
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
-    # url(r'^kuso/', include('docker_django.apps.kuso_wifi_server.urls')),
+    url(r'^', include('kuso_wifi_server.urls', namespace='kuso_wifi_server')),
     # url(r'^', include('docker_django.apps.kuso_wifi_server')),
 ]
